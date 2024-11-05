731a75e872ff4d0dd86bc1c1b4825ab0c8eca162
El-somm <elesesommerfeld@outlook.com>
2024-11-04 19:26:51 -0800

    backup

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+export const geoApiOptions = {
+	method: "GET",
+	headers: {			
+	  "X-RapidAPI-Key": "289cf15259msh43ebb8a1b83aa4ep192c6cjsn4ec1e8abd891",
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "e692a521d0a6125d2f501a13f2aadc82";
